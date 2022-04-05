c05cf9a6317a1757e4e2d7571d9d9afe450b015a
Ezequiel De La Rosa <erosarosario@cifpfbmoll.eu>
2022-04-05 20:18:16 +0200

    feat: add method interface for general controller regulation mode

--- im-cold/src/main/java/edu/poniperro/interfaces/Regulator.java
+++ im-cold/src/main/java/edu/poniperro/interfaces/Regulator.java
@@ -5,4 +5,5 @@ import edu.poniperro.entities.Room;
 public interface Regulator {
     void activateHeater(double maxTemp, double minTemp, Room room, Heater heater, Measurament thermometer);
     void deactivateHeater(double maxTemp, double minTemp, Room room, Heater heater, Measurament thermometer);
+    void regulate(double minTemp, double maxTemp, Room room, Heater heater, Measurament thermometer);
 }
