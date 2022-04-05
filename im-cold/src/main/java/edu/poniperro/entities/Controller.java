c4ea592f47526d7a24701b04c9a4f4bf1ac38465
Ezequiel De La Rosa <erosarosario@cifpfbmoll.eu>
2022-04-05 20:05:19 +0200

    feat: finish deactivateHeater() and activateHeater() to warn or viceversa room temperature

--- im-cold/src/main/java/edu/poniperro/entities/Controller.java
+++ im-cold/src/main/java/edu/poniperro/entities/Controller.java
@@ -6,21 +6,26 @@ import edu.poniperro.interfaces.Regulator;
 
 public class Controller implements Regulator{
 
-    public Controller(double maxTemp, double minTemp, Room room, Heater heater, Measurament thermometer){};
+    public Controller(){};
+    // public Controller(double maxTemp, double minTemp, Room room, Heater heater, Measurament thermometer){};
 
     @Override
     public void deactivateHeater(double maxTemp, double minTemp, Room room, Heater heater, Measurament thermometer) {
-        while(thermometer.readTemperature(room) > maxTemp && !heater.getStatus()) {
+        while(thermometer.readTemperature(room) > maxTemp) {
+                heater.setStatus(false);
                 room.decreaseRoomTemperature();
+                System.out.println("Enfriando..." + thermometer.readTemperature(room));
+
         }
     }
 
     public void activateHeater(double maxTemp, double minTemp, Room room, Heater heater, Measurament thermometer) {
-        while(thermometer.readTemperature(room) < minTemp && heater.getStatus() == true) {
+        while(thermometer.readTemperature(room) < maxTemp) {
             heater.warm(room);
-            // if (thermometer.readTemperature(room) == maxTemp) {
-            //     break;
-            // }
+            System.out.println("Calentando..." + thermometer.readTemperature(room));
+            if (thermometer.readTemperature(room) == maxTemp) {
+                break;
+            }
         }
     }
 
