bc6149ba022b1027db6865fb5fb869f803fb5611
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-04-11 20:05:23 +0200

    feat: implement Jedi object as heater

--- im-cold/src/main/java/edu/poniperro/App.java
+++ im-cold/src/main/java/edu/poniperro/App.java
@@ -2,6 +2,7 @@ package edu.poniperro;
 
 import edu.poniperro.entities.Controller;
 import edu.poniperro.entities.Furnace;
+import edu.poniperro.entities.Jedi;
 import edu.poniperro.entities.Room;
 import edu.poniperro.entities.Thermometer;
 import edu.poniperro.interfaces.Heater;
@@ -12,32 +13,30 @@ import edu.poniperro.interfaces.Regulator;
  * Hello world!
  *
  */
-public class App
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
 
         double maxTemp = 21;
         double minTemp = 15;
 
         Room livingRoom = Room.getInstance();
-        livingRoom.setRoomTemperature(30);
+        livingRoom.setRoomTemperature(15);
 
         Measurament thermometer = new Thermometer();
 
         Heater furnace = new Furnace();
         furnace.setStatus(true);
 
-
-        // Room 15Cn
-        // Termometro
-        // Horno
-        // Control
-
         Regulator distanceController = new Controller();
+        distanceController.regulate(minTemp, maxTemp, livingRoom, furnace, thermometer);
 
-        distanceController.regulate(maxTemp, minTemp, livingRoom, furnace, thermometer);
+        System.out.println( "Arrancando..." );
+        distanceController.regulate(minTemp, maxTemp, livingRoom, furnace, thermometer);
 
+        Jedi yoda = new Jedi();
+        System.out.println( "\nArrancando a Yoda: " );
+        distanceController.regulate(minTemp, maxTemp, livingRoom, yoda, thermometer);
+        yoda.speak();
 
     }
 }
